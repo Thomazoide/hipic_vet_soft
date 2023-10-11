b339db44daf9c1a8cd1f36e9e2b7894132dbd78a
Thomazoide <t.cannobbioabarca@uandresbello.edu>
2023-10-10 23:17:44 -0300

    avance 11-10

--- backend/controllers/usuarios.control.js
+++ backend/controllers/usuarios.control.js
@@ -8,16 +8,29 @@ userCtrl.getUsers = async (req, res) => {
 }
 
 userCtrl.setUser = async (req, res) => {
-    const {tipo, nombre, rut, email, cell, pass, cod_equipo, corrales_en_posesion} = req.body
-    const newUser = new Usuarios({
-        tipo: tipo,
-        rut: rut,
-        pass: pass,
-        cod_equipo: cod_equipo,
-        corrales_en_posesion: corrales_en_posesion
-    })
-    await newUser.save()
-    res.json(newUser)
+    
+    try{
+        const {tipo, nombre, rut, email, cell, pass, corrales_en_posesion} = req.body
+        if(rut.length < 9){
+            throw Error('Rut invalido')
+        }
+        console.log(rut)
+        let cod_equipo = ''
+        let auxRut = rut.split('-')[0]
+        auxRut = auxRut.substr((auxRut.length-3), auxRut.length)
+        let auxLn = nombre.split(' ')[1]
+        auxLn = auxLn.substr(0, 4)
+        auxLn = auxLn.toLowerCase()
+        cod_equipo = `${auxRut}${auxLn}`
+        console.log(cod_equipo)
+        const newUser = await Usuarios.signup(tipo, nombre, rut, email, cell, pass, cod_equipo, corrales_en_posesion)
+        console.log('Object::: ', corrales_en_posesion)
+        
+        res.status(200).json({rut, newUser})
+    }catch(err){
+        res.status(400).json(err.message)
+        console.log(err.message)
+    }
 }
 
 module.exports = userCtrl
--- backend/models/usuarios.js
+++ backend/models/usuarios.js
@@ -1,4 +1,6 @@
 const {Schema, model} = require('mongoose')
+const bcrypt = require('bcrypt')
+const validator = require('validator')
 const Usuarios = new Schema({
     tipo: {
         type: String,
@@ -22,5 +24,24 @@ const Usuarios = new Schema({
     },
     corrales_en_posesion: [String],
 })
+
+Usuarios.statics.signup = async function(tipo, nombre, rut, email, cell, pass, cod_equipo, corrales_en_posesion){
+    if(!tipo || !rut || !pass || !cod_equipo){
+        throw Error('Flatan valores por entregar...')
+    }
+    if(email){
+        if(!validator.isEmail(email)){
+            throw Error('Email invalido...')
+        }
+    }
+    const exists = await this.findOne({rut})
+    if(exists){
+        throw Error('usuario ya existe')
+    }
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(pass, salt)
+    const user = await this.create({tipo, nombre, rut, email, cell, pass: hash, cod_equipo, corrales_en_posesion})
+    return user
+}
 
 module.exports = model("usuarios", Usuarios)
