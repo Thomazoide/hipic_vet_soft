ee4cf4d611bfcb7e4f159d41afa64dafb59a7d7d
Thomazoide <t.cannobbioabarca@uandresbello.edu>
2023-10-11 14:44:22 -0300

    avance 11/10

--- backend/src/database.js
+++ backend/src/database.js
@@ -2,7 +2,7 @@ const {connect} = require('mongoose')
 
 async function conectarBD(){
     try{
-        await connect('mongodb+srv://Thomazoide:Thom1232!@mastercluster.hasjpif.mongodb.net/Club_Hipico', {useNewUrlParser: true, useUnifiedTopology: true})
+        await connect(process.env['MONGO_LOGIN'], {useNewUrlParser: true, useUnifiedTopology: true})
         console.log('conectado correctamente a BBDD')
     }catch(err){
         console.log('Error al conectar... \n*************',err,'\n*************')
