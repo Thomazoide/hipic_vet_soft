48ecf32781af11674b3366675db31cd8e05a274b
Thomazoide <t.cannobbioabarca@uandresbello.edu>
2023-12-14 19:53:23 -0300

    updates

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,14 +8,12 @@ app.set('port', process.env.PORT || 4444)
 app.use(express.json())
 
 
-app.use(cors({
-    origin: ['http://34.230.2.165:5173', 'http://localhost:4444', 'https://tsi97wk2d8.execute-api.us-east-1.amazonaws.com', 'https://hipic-vet-soft.vercel.app/']
-}))
+app.use(cors())
 
 
 /* Seccion APIs */
 
-app.use('/api/users', require('./../routes/usuarios.routes'), cors())
+app.use('/api/users', require('./../routes/usuarios.routes'))
 app.use('/api/caballos', require('./../routes/caballos.routes'))
 app.use('/api/fichas', require('./../routes/ficha_medica.routes'))
 app.use('/api/login', require('./../routes/login.routes'))
